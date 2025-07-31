dffe3c8136c86e0836fcdb91978c2199e3acf397
Quitzel <THartman7.42@gmail.com>
2025-07-31 10:36:57 -0400

    Several things added, includes many bugs
    Level updated
    grapple hook added
    raycast gun fixed
    Angel is animated now

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,14 +1,29 @@
-extends CharacterBody3D
+extends AnimatableBody3D
 
 signal Reload
+signal hasNoAmmo
+signal hasAmmo
 
+
+
+var playerHasAmmo = true
 func destroy():
-	emit_signal("Reload")
 	hide()
 
 
 func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
-	#if Input.is_action_just_pressed("fireGun"):
-		#emit_signal("Reload")
-		#destroy()
+	if Input.is_action_just_pressed("fireGun") && playerHasAmmo:
+		emit_signal("Reload")
+		playerHasAmmo = true
+		destroy()
+	else:
 		pass
+
+
+func _on_player_has_no_ammo() -> void:
+	playerHasAmmo = false
+
+
+
+func _on_player_has_ammo() -> void:
+	playerHasAmmo = true
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,17 +2,19 @@ extends CharacterBody3D
 
 @export var hasBullet = true
 var SPEED = 15.0
-const JUMP_VELOCITY = 9.8
+const JUMP_VELOCITY = 7
+signal hasAmmo
+signal hasNoAmmo
 
 func setCamFOV(newFOV):
 	$Camera3D.fov = newFOV
 	
 func gunPosADS():
-	$Camera3D/Gun.position = Vector3(0,-0.1,-0.4) 
-	$Camera3D/Gun.rotate_y(0.174533)
+	$Camera3D/Gun.position = Vector3(0,-0.12,-0.4) 
+	$Camera3D/Gun.rotation.y = PI
 func gunPosStd():
 	$Camera3D/Gun.position = Vector3(0.33,-0.15,-0.4)
-	$Camera3D/Gun.rotate_y(-0.174533)
+	$Camera3D/Gun.rotation.y = PI * (-19/18)
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	var mouse_pos = get_viewport().get_mouse_position()
@@ -42,12 +44,15 @@ func turnCamera(delta):
 func _ready():
 	pass
 
-func fireShot() -> Object:
-	$Camera3D/RayCast3D.transform.origin = $Camera3D.position
-	$Camera3D/RayCast3D.look_at($Camera3D.rotation)
-	$Camera3D/RayCast3D.force_raycast_update()
-	return $Camera3D/RayCast3D.get_collider()
+func fireShot() -> void:
+	pass
+	#emit_signal("playerHasNoAmmo")
+	#$Camera3D/RayCast3D.transform.origin = $Camera3D.position
+	#$Camera3D/RayCast3D.look_at($Camera3D.rotation)
+	#$Camera3D/RayCast3D.force_raycast_update()
+	#return $Camera3D/RayCast3D.get_collider()
 
 
 func _on_enemy_reload() -> void:
 	hasBullet = true
+	emit_signal("hasAmmo")
